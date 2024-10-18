239bd5572e6110858b0808f0770aac51fd366d9a
Fernando <fgaston@crm.cat>
2024-10-18 21:05:47 +0200

    Added more species

--- extract_images.py
+++ extract_images.py
@@ -8,18 +8,24 @@ from selenium.webdriver.support import expected_conditions as EC
 
 
 from bs4 import BeautifulSoup
-# import requests
-
+import json 
 
 
 if __name__=="__main__":
     species = [("Llissa Vera", 250654), ("Orada", 68960), ("Mabre", 48596),("Sarg", 12553),
     ("Esparrall", 34841),("Verada",34798),("Oblada",240400),("Salpa",34805),("Boga",246899),
     ("Aranyó",245401),("Moll de roca",35125),("Escórpora",34965),("Serrà",13755),("Anfós",246069),
-    ("Vaca serrana",13765),("Joell",240394),("LLobarro",34837),("Xucladits",243584),("Castanyoletes",35225),("Fadrí",35194),("Juliola",34835),("Espatarc",35216 ),("Planxeta",34986),("Roqueret",240397),("Tacó",35282)]
+    ("Vaca serrana",13765),("Joell",240394),("LLobarro",34837),("Xucladits",243584),
+    ("Castanyoletes",35225),("Fadrí",35194),("Juliola",34835),("Espatarc",35216 ),
+    ("Planxeta",34986),("Roqueret",240397),("Tacó",35282),("Bavosa Menuda", 34983),
+    ("Bavosa de Plomall", 35250), ("Futarra", 34878), ("Dormilega", 35044), ("Capsigrany", 34989),
+    ("Llepissós", 35244), ("Cabot de Roca", 34847), ("Bavosa Morruda",240399)]
     speciesToImg={}
 
-    for name, taxon_id in species[:10]:
+    with open('Data/fish.json', 'r') as file:
+        data = json.load(file)
+
+    for name, taxon_id in species:
         print(f"Scraping images for {name} with taxon_id: {taxon_id}")
         user = "xasalva"
         taxon_id = str(taxon_id)
@@ -44,7 +50,7 @@ if __name__=="__main__":
                 observations.append(observation_url)
 
         images = []
-        for observation_url in list(set(observations))[:5]:
+        for observation_url in list(set(observations)):
             print(observation_url)
             driver.get(observation_url)
             WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'image-gallery-image')))
