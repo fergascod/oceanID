a0d14f88f44c591df697c21affbbf61f674b960e
Fernando <fgaston@crm.cat>
2024-10-25 09:35:29 +0200

    added new modes

--- modes.py
+++ modes.py
@@ -1,4 +1,4 @@
-test=[
+test = [
 "Sarg",
 "Mabre",
 "Orada",
@@ -9,7 +9,61 @@ test=[
 "Boga",
 ]
 
+tot = [
+  "Llissa Vera",
+  "Orada",
+  "Mabre",
+  "Sarg",
+  "Esparrall",
+  "Verada",
+  "Oblada",
+  "Salpa",
+  "Boga",
+  "Aranyó",
+  "Moll de roca",
+  "Escórpora",
+  "Serrà",
+  "Anfós",
+  "Vaca serrana",
+  "Joell",
+  "LLobarro",
+  "Xucladits",
+  "Castanyoletes",
+  "Fadrí",
+  "Juliola",
+  "Espatarc",
+  "Planxeta",
+  "Roqueret",
+  "Tacó",
+  "Bavosa Menuda",
+  "Bavosa de Plomall",
+  "Futarra",
+  "Dormilega",
+  "Capsigrany",
+  "Llepissós",
+  "Cabot de Roca",
+  "Bavosa Morruda"
+]
+
+joell_boga = [
+  "Boga",
+  "Joell"
+]
+
+bavoses_i_cabots = [
+  "Bavosa Menuda",
+  "Bavosa de Plomall",
+  "Futarra",
+  "Dormilega",
+  "Capsigrany",
+  "Llepissós",
+  "Cabot de Roca",
+  "Bavosa Morruda" 
+]
 
 modes={
 "test":test,
+"Tots els peixos de la miniguia": tot,
+"Joell o boga?": joell_boga,
+"Bavoses i cabots": bavoses_i_cabots
 }
